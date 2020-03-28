fce2832975243b578b543416b1c16dc8c00638b9
poteto033 <nhnhnnhn033@gmail.com>
2020-03-28 14:55:28 +0900

    カテゴリー部分の実装
    ・カテゴリー部分を実装した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,37 @@
 class ItemsController < ApplicationController
-
   def index
     
   end
+
   def new
-    
+    @item = Item.new
+    @category=Category.all.where(ancestry: nil)
+  end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+ def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+ end
+
+
+  def create
+    Item.create(item_params)
   end
   def show
   end
 
   def edit
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:text,:item_status,:price,:delivery_charge,:delivery_area,:delivery_days,:category_id,:brand_id,:solder_id,:buyer_id)
+    
+  end
 end
+
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root "items#index"
-  resources :items, only: [:show , :edit, :new]
-  resources :users, only: [:new]
+  resources :items, only: [:show , :edit, :new,:create]do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+  
 end
