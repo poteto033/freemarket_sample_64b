b9a97a1221424ebc733617789045425f51c43c9b
poteto033 <nhnhnnhn033@gmail.com>
2020-03-29 22:04:30 +0900

    データベース修正
    ・カテゴリーテーブルのancestryカラムにinxexオプションを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20200317060731) do
     t.string   "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
