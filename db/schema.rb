3f136ad2d8e8bbc80675658dcd0008f8d49f6b86
poteto033 <nhnhnnhn033@gmail.com>
2020-04-28 17:33:40 +0900

    画像の表示のエラー対応
    ・画像が表示できないエラーを解消した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200316092427) do
+ActiveRecord::Schema.define(version: 20200314053939) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
