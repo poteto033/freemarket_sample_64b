d7a93cca92f3ac133b26f3b242ba9a849b8204b6
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-05-06 22:59:02 +0900

    カード機能のテスト

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -9,18 +9,18 @@ describe Card do
       expect(card).to be_valid
     end
 
-    # it 'is invalid without customer_id' do
-    #   user = create(:user)
-    #   card = build(:card, user_id: user.id, customer_id: "")
-    #   card.valid?
-    #   expect(card.errors[:customer_id]).to include("を入力してください")
-    # end
+    it 'is invalid without customer_id' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
 
-    # it 'is invalid without card_id' do
-    #   user = create(:user)
-    #   card = build(:card, user_id: user.id, card_id: "")
-    #   card.valid?
-    #   expect(card.errors[:card_id]).to include("を入力してください")
-    # end
+    it 'is invalid without card_id' do
+      user = create(:user)
+      card = build(:card, user_id: user.id, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("を入力してください")
+    end
   end
 end
